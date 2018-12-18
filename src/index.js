2b1ffc711c17b11e2fbc821a62b3cfb9b9c1f71f
César Chávez <design@pinata.tech>
2018-12-17 21:08:22 -0600

    Transitions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import './index.scss';
 import * as serviceWorker from './serviceWorker';
 
@@ -15,15 +16,23 @@ import Alfareria from './Views/Alfareria';
 
 ReactDOM.render(
     <BrowserRouter>
-        <Switch>
-            <Route exact path='/' component={Introduction}/>
-            <Route exact path='/home' component={Home}/>
-            <Route exact path='/cara-diablo' component={CaraDiablo}/>
-            <Route exact path='/cobre' component={Cobre}/>
-            <Route exact path='/canteria' component={Canteria}/>
-            <Route exact path='/alfareria' component={Alfareria}/>
-            <Route component={()=> (<h1>Nothing here!</h1>)}/>
-        </Switch>
+        <Route
+                render={({ location }) => (
+                  <TransitionGroup>
+                    <CSSTransition key={location.key} timeout={300} classNames='fade'>
+                      <Switch>
+                        <Route exact path='/' component={Introduction}/>
+                        <Route exact path='/home' component={Home}/>
+                        <Route exact path='/cara-diablo' component={CaraDiablo}/>
+                        <Route exact path='/cobre' component={Cobre}/>
+                        <Route exact path='/canteria' component={Canteria}/>
+                        <Route exact path='/alfareria' component={Alfareria}/>
+                        <Route component={()=> (<h1>Nothing here!</h1>)}/>
+                      </Switch>
+                    </CSSTransition>
+                  </TransitionGroup>
+                )}
+              />
     </BrowserRouter>
     , document.getElementById('root'));
 
